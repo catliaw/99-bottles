99adcfed911b1cddca88b6dbda06ab2a76f227d7
Catherine Liaw <cat.liaw@gmail.com>
2017-09-07 18:05:11 -0700

    Third test (test_verse_2) passed. Added if statement to case of 2 bottles, so 1 bottle would be singular not plural.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -6,10 +6,17 @@ class Bottles
   # end
 
   def verse(num_bottles)
-    multiline_verse = <<-VERSE
+    if num_bottles == 2
+      multiline_verse = <<-VERSE
+#{num_bottles} bottles of beer on the wall, #{num_bottles} bottles of beer.
+Take one down and pass it around, #{num_bottles - 1} bottle of beer on the wall.
+VERSE
+    else
+      multiline_verse = <<-VERSE
 #{num_bottles} bottles of beer on the wall, #{num_bottles} bottles of beer.
 Take one down and pass it around, #{num_bottles - 1} bottles of beer on the wall.
 VERSE
+    end
     puts multiline_verse
     return multiline_verse
   end
